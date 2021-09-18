b91cf2e3749e39c074938f284a2b12f4b2eb6ddc
TRABZI Mohammed Amine <amine.trabzi@yahoo.com>
2021-09-18 14:16:45 +0200

    upd

--- demo.py
+++ demo.py
@@ -25,14 +25,26 @@ def home():
             return render_template('index.html',message=error_msg)
 
 
-@app.route('/GETfootball',methods=['GET','POST'])
+@app.route('/GETfootball',methods=['GET'])
 def foot():
     return render_template('foot.html')
 
 
-@app.route('/structur',methods=['GET','POST'])
+@app.route('/structur',methods=['GET'])
 def func():
     return render_template('structur.html')
+
+@app.route('/signup',methods=['GET','POST'])
+def signup():
+    msg='Please signe up !'
+    if request.method=='GET':
+        return render_template('signup.html',message=msg)
+    else:
+        username=request.form['username'] 
+        password=request.form['passeword']
+        favorit_color=request.form['favorit_color']
+        msg=model.signup(username,password,favorit_color)
+        return render_template('signup.html',message=msg)
 
 if __name__ == '__main__':
     app.run(port=7000,debug=True)
--- model.py
+++ model.py
@@ -30,3 +30,24 @@ def check_pw(username):
     connection.close()
 
     return passeword
+
+def signup(username,passeword,favorit_color):
+    connection=sqlite3.connect('flask_tuto.db',check_same_thread=False)
+    cursor=connection.cursor()
+
+    cursor.execute(
+        """ SELECT passeword FROM users WHERE username='{username}';""".format(username=username)
+    )
+    exist=cursor.fetchone()[0]
+    
+    if exist is None:
+        cursor.execute(
+        """ INSERT INTO users (username,passeword,favorit_color)VALUES('{username}','{passeword}','{favorit_color}');""".format(username=username,passeword=passeword,favorit_color=favorit_color))
+        connection.commit()
+        cursor.close()
+        connection.close()
+
+    else:
+        return('User already existed !!!')
+
+    return 'You have signed up successfully !!!'
